d780a2ffac1251d5dc3597c5b187882b58699a43
Dongyadong <820020632@qq.com>
2020-04-01 23:13:09 +0800

    第一次提交

---
+++ HW3/Group16/hw3_1720966.py
@@ -0,0 +1,23 @@
+k = -9
+if k>= 0:
+  k1 = str(k)+"正数"
+  print(k1)  
+else:
+    print(str(k) + "负数")
+
+
+
+'''
+2k = -9                              变量不能以数字开头
+
+  if 2k>= 0:                         if前面不能有空格
+
+    with = 2k +"正数"                with不能当变量名字，with是关键字，改为k，'int' and 'str'不能用+来拼接
+                                      把k转换为str类型
+        print(with)                  with改为k1 
+
+    else:                           else应该与if对齐，同一级别代码
+
+    print(2K+ '负数")               不能用'"来表示字符串，改为""，2K与2k不同，变量区分大小写
+
+'''
