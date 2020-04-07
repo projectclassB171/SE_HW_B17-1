326f2594531355012ead418265f4e0af87c12551
wangziyang-colud <1124270351@qq.com>
2020-04-07 18:45:19 +0800

    作业4

---
+++ HW4/Group3/hw4_1720299_1.py
@@ -0,0 +1,8 @@
+for g in range(0,100):
+    for m in range(0,100):
+               x=100-g-m 
+               if x>=0 and 5*g+3*m+x/3==100:
+                   print("公鸡%d只、母鸡%d只、小鸡%d只"%(g,m,x))
+                 
+ 
+                    
