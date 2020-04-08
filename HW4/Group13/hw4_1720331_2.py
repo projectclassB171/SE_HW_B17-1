1f63fb84ea8fe9627fdaf136cfd366aa056d212a
1720331zgp <1720331zgp@example.com>
2020-04-08 18:38:06 +0800

    second commit

---
+++ HW4/Group13/hw4_1720331_2.py
@@ -0,0 +1,9 @@
+# coding=gbk
+ji=0
+tu=30-ji
+for ji in range(1,30):
+    for tu in range(1,30):
+         if ji+tu == 30:
+            if tu*4+ji*2 == 90:
+              print("╪╕сп:",ji,"мцвссп:",tu)
+
