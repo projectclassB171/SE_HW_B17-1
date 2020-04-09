2e0d70f328f2f6d40624795168379678e3eec97b
Your Name <you@example.com>
2020-04-09 12:48:05 +0800

    HW4

---
+++ HW4/Group2/hw4_1520991_1.py
@@ -0,0 +1,11 @@
+﻿money=100
+score=0
+for x in range(21):
+    for y in range(34):
+        for z in range(301): 
+            score=5 * x + 3 * y + z / 3
+            if score==money and x+y+z==100:
+                print('公鸡是%s只，母鸡是%s只，小鸡是%s只'%(x,y,z))
+
+
+
