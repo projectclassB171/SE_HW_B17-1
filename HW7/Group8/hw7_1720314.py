adbc1d495361befe4a77201eb191cec75a1a0208
Hannah.奥利维亚 <hannah@MacBook-Pro.local>
2020-04-30 22:14:22 +0800

    first

---
+++ HW7/Group8/hw7_1720314.py
@@ -0,0 +1,30 @@
+#1.编写函数，接收两个正整数作为参数，返回一个元组，其中第一个元素为最大公约数，第二个元素为最小公倍数。
+def demo(x,y):
+     if x>y:
+             x,y=x,y
+     p = x*y
+     while x!=0:
+             r = x%y
+             y=n
+             y=r
+     return(n,p//x)
+print(demo(3,300))
+
+#2.编写函数，接受一个字符串作为参数，计算并打印传入字符串中数字，字母，空格，以及其它的个数。
+def count(str):
+    number = 0
+    letter = 0
+    space = 0
+    others = 0
+    for i in s:
+        if i.isdigit():
+            number += 1
+        elif i.isalpha():
+            letter += 1
+        elif i.isspace():
+            space += 1
+        else:
+            others += 1
+    print('数字的个数：{}\t字母的个数：{}\t空格的个数：{}\t其他的个数：{}'.format(number, letter, space, others))
+count(input("请输入一个字符串："))
+
