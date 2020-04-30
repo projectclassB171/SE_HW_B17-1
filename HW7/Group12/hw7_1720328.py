4bf18576cbb79a6f350a96c7c7267c3a538836c2
yancaizhen <1918228109@qq.com>
2020-04-30 11:07:10 +0800

    1

---
+++ HW7/Group12/hw7_1720328.py
@@ -0,0 +1,35 @@
+#<1>编写函数，接收两个正整数作为参数，返回一个元组，其中第一个元素为最大公约数，第二个元素为最小公倍数。
+
+def demo(a,b):
+    if a>b:
+        a,b=b,a
+    p=a*b
+    while a!=0:
+        s=b%a
+        b=a
+        a=s
+    return (b,int(p/b))
+print(demo(9,19))
+#运行结果:(1,171)
+
+#<2>编写函数，接受一个字符串作为参数，计算并打印传入字符串中数字，字母，空格，以及其它的个数。
+def count(s):
+    numb=0
+    alpha=0
+    space=0
+    other=0
+    for i in s:
+        if i.isdigit():
+            numb +=1
+        elif i.isalpha():
+            alpha +=1
+        elif i.isspace():
+            space +=1
+        else:
+            other +=1
+    print('数字:{},字母:{},空格:{},其他:{}'.format(numb,alpha,space,other))
+count(input("请输入一个字符串："))
+#运行结果:
+#请输入一个字符串：11823%%5,ahssui  000
+#数字:9,字母:6,空格:2,其他:3
+
